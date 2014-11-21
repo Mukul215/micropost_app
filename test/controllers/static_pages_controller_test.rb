e64cd9ea5d6e3c444ad8e0b4ec9f95c965333128
Mukul B. Patel <Mukul215@gmail.com>
2014-11-20 20:36:02 -0500

    DRYed up title and added contact page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,25 +1,36 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  # Creating a instance variable to DRY up code for base title
+  def setup
+    @base_title = "Ruby on Rails Sample App"
+  end
+
   # <--- This is how it should be read --->
   # Let’s test the Home page by issuing a GET request to the home action 
   # and then making sure we receive a ‘success’ status code in response.
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Sample App"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Sample App"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Sample App"
+    assert_select "title", "About | #{@base_title}"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
+  
 end
