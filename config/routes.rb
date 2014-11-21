723bee0cdf2e7b6b561534a89913392e5caab0c7
Mukul B. Patel <Mukul215@gmail.com>
2014-11-20 19:57:19 -0500

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
+  # These routes are from the static_pages controller
+  root  'static_pages#home'
+  get   'static_pages/help'
+  get   'static_pages/about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,25 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  # <--- This is how it should be read --->
+  # Let’s test the Home page by issuing a GET request to the home action 
+  # and then making sure we receive a ‘success’ status code in response.
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Sample App"
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Sample App"
   end
 
 end
