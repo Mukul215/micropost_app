bbd545158fecf82ee607793d456615bca262ca56
Mukul B. Patel <Mukul215@gmail.com>
2014-11-25 15:08:40 -0500

    added test application helper

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,13 +3,14 @@ require 'test_helper'
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
   # test to check if links work properly
-  test 'layout_links' do
+  test "layout links" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select   "a[href=?]", root_path, count: 2
-    assert_select   "a[href=?]", help_path
-    assert_select   "a[href=?]", about_path
-    assert_select   "a[href=?]", contact_path
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
-
 end
