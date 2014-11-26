cb8f492770a96f257ec8f393695d2ebe85652728
Mukul B. Patel <Mukul215@gmail.com>
2014-11-26 15:46:59 -0500

    Added sign up form for new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
+
   def show
+    # this is used to find users by their ID
     @user = User.find(params[:id])
   end
 
   def new
+    # used to create new user
+    @user = User.new
   end
 end
