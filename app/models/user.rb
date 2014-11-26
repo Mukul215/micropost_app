384b801efbe4e67b95fc6a12b1a7252d2e97e308
Mukul B. Patel <Mukul215@gmail.com>
2014-11-26 15:11:41 -0500

    Added gravatar functionality to user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   # validates(:name, presence: true) is acceptable
   validates :name,  presence: true, length: { maximum: 50 }
   # email regular expression needed to valid emails
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     # unique validation is case sensitive
