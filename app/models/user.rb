e2ad155e42ed563959cb38bb7ad094e2f6d66f9b
Mukul B. Patel <Mukul215@gmail.com>
2014-12-29 17:59:12 -0500

    Completed user following and feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -99,7 +99,10 @@ class User < ActiveRecord::Base
   # defines a proto-feed
   # see "Following Users" for full implementation
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
 
   # follows a user
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -77,4 +77,22 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.authenticated?(:remember, ' ')
   end
 
+  test "feed should have the right posts" do
+    mukul = users(:mukul)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # posts from followed user
+    lana.microposts.each do |post_following|
+      assert mukul.feed.include?(post_following)
+    end
+    # posts from self
+    mukul.microposts.each do |post_self|
+      assert mukul.feed.include?(post_self)
+    end
+    # posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not mukul.feed.include?(post_unfollowed)
+    end
+  end
+
 end
